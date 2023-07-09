708bad80cf2e2c5cdb8a40fe3830e49c73372d2e
mgblanco10 <mgblanco1001@gmail.com>
2023-07-09 08:14:39 +0200

    finish json server and wip maquetacion y estilos aunque todo totalmente funcional

--- app/src/components/FavoriteMessage/FavoriteMesage.jsx
+++ app/src/components/FavoriteMessage/FavoriteMesage.jsx
@@ -1,100 +1,10 @@
-// // import React, { useState, useEffect } from 'react';
-// // import './FavoriteMessage.css'
-// // import axios from 'axios';
-
-// // function FavoriteMesage() {
-// //   const [cardData, setCardData] = useState([]);
-
-// //   useEffect(() => {
-// //     axios.get('http://localhost:3000/cardGroups')
-// //       .then(response => {
-// //         const data = response.data;
-// //         setCardData(data);
-// //       })
-// //       .catch(error => {
-// //         console.error('Error al obtener los datos:', error);
-// //       });
-// //   }, []);
-
-// //   return (
-// //     <div className='favoritePage'>
-// //       {cardData.map((cardGroup, index) => (
-// //         <div key={index}>
-// //           <h3>Meanings:</h3>
-// //           <ul>
-// //             {cardGroup.cards.map((card, index) => (
-// //               <li key={index}>{card.meaning}</li>
-// //               ))}
-// //           </ul>
-// //               <h3>Message:</h3>
-// //               <p>{cardGroup.message}</p>
-// //         </div>
-// //       ))}
-// //     </div>
-// //   );
-// // }
-
-// // export default FavoriteMesage;
-
-
-
-
-// import React, { useState, useEffect } from 'react';
-// import './FavoriteMessage.css';
-// import axios from 'axios';
-
-// function FavoriteMessage() {
-//   const [cardData, setCardData] = useState([]);
-
-//   useEffect(() => {
-//     axios
-//       .get('http://localhost:3000/cardGroups')
-//       .then((response) => {
-//         const data = response.data;
-//         setCardData(data);
-//       })
-//       .catch((error) => {
-//         console.error('Error al obtener los datos:', error);
-//       });
-//   }, []);
-
-//   function formatDate(timestamp) {
-//     const date = new Date(timestamp);
-//     const day = date.getDate();
-//     const month = date.getMonth() + 1;
-//     const year = date.getFullYear();
-//     return `${day}/${month}/${year}`;
-//   }
-
-//   return (
-//     <div className="favoritePage">
-//       {cardData.map((cardGroup, index) => (
-//         <div key={index}>
-//           <h3>Date: {formatDate(cardGroup.date)}</h3>
-//           <h3>Meanings:</h3>
-//           <ul>
-//             {cardGroup.cards.map((card, index) => (
-//               <li key={index}>{card.meaning}</li>
-//             ))}
-//           </ul>
-//           <h3>Message:</h3>
-//           <p>{cardGroup.message}</p>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// }
-
-// export default FavoriteMessage;
-
-
-
 import React, { useState, useEffect } from 'react';
 import './FavoriteMessage.css';
 import axios from 'axios';
 
 function FavoriteMessage() {
   const [cardData, setCardData] = useState([]);
+  const [selectedCards, setSelectedCards] = useState([]);
 
   useEffect(() => {
     axios
@@ -116,16 +26,30 @@ function FavoriteMessage() {
     return `${day}/${month}/${year}`;
   }
 
+  const handleDelete = async (index) => {
+    try {
+      await axios.delete(`http://localhost:3000/cardGroups/${cardData[index].id}`);
+      const updatedCardData = [...cardData];
+      updatedCardData.splice(index, 1);
+      setCardData(updatedCardData);
+    } catch (error) {
+      console.error('Error al eliminar los datos:', error);
+    }
+  };
+
+  const filteredCardData = cardData.filter((_, index) => !selectedCards.includes(index));
+
   return (
     <div className="favoritePage">
-      {cardData.map((cardGroup, index) => (
+      {filteredCardData.map((cardGroup, index) => (
         <div key={index} className="cardGroupContainer">
+          <button className="buttonDeleteFavorites"  onClick={() => handleDelete(index)}>X</button>
           <div className="cardGroupWindow">
             <h3>Date: {formatDate(cardGroup.date)}</h3>
             <h3>Meanings:</h3>
-            <ul>
+            <ul className='meaningList'>
               {cardGroup.cards.map((card, index) => (
-                <li key={index}>{card.meaning}</li>
+                <li className='meaningList' key={index}>{card.meaning}</li>
               ))}
             </ul>
             <h3>Message:</h3>
@@ -138,5 +62,3 @@ function FavoriteMessage() {
 }
 
 export default FavoriteMessage;
-
-
